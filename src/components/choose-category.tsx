1c2c41356856bc3fff8afcb6ab736290f34f392f
felipe <jonathanfelipe0111@hotmail.com>
2024-09-24 20:25:49 -0500

    feat: added category title in the categories section with backdrop-blur and black text

--- src/components/choose-category.tsx
+++ src/components/choose-category.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useGetCategory } from '@/api/getPtoducts'
 import { CategoryType } from '@/types/category';
-import { ProductType } from '@/types/product';
 import { ResponseType } from '@/types/response'
 import Link from 'next/link';
 
@@ -15,20 +14,23 @@ const ChooseCategory = () => {
     return (
         <div className='max-w-6xl py-4 mx-auto sm:py-16 sm:px-24'>
             <h3 className='px-6 pb-4 text-3xl sm:pb-8'>Elige tu categoria favorita</h3>
-            <div className='grid gap-5 sm:grid-cols-3 text-black'>
+            <div className='grid gap-5 sm:grid-cols-3'>
                 {
                     !loading && result !== undefined && (
                         categories.map((category: CategoryType) => (
                                 <Link
                                     key={category.id}
                                     href={`/category/${category.slug}`}
-                                    className='realtive max-w-xs mx-auto overflow-hidden bg-no-repeat bg-cover rounded-lg cursor-pointer'
+                                    className='relative max-w-xs mx-auto overflow-hidden bg-no-repeat bg-cover rounded-lg cursor-pointer'
                                 >
                                     <img
                                         src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${category.mainImage.url}`}
                                         alt={category.categoryName}
                                         className='max-w-[270px] transition duration-300 ease-in-out rounded-lg hover:scale-110'
-                                    />
+                                    /> 
+                                    <p className='absolute w-full py-2 text-lg font-bold text-center bottom-5 backdrop-blur-lg text-black'>
+                                        {category.categoryName}
+                                    </p>
                                 </Link>
                         ))
                     )
